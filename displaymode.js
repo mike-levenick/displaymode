c4ea694b41fe9243427a62ca8f0350b5f17af62d
Mike Levenick <michael.levenick@gmail.com>
2023-01-20 23:18:00 -0600

    Update displaymode.js

--- displaymode.js
+++ displaymode.js
@@ -2,12 +2,14 @@ Hooks.on('ready', () => {
     $('#logo').click(ev => {
         window.hideUI = !window.hideUI;
         if (window.hideUI) {
+            $('#logo').hide();
             $('#sidebar').hide();
             $('#navigation').hide();
             $('#controls').hide();
             $('#players').hide();
             $('#hotbar').hide();
         } else {
+            $('#logo').show();
             $('#sidebar').show();
             $('#navigation').show();
             $('#controls').show();
@@ -15,6 +17,12 @@ Hooks.on('ready', () => {
             $('#hotbar').show();
         }
     });
+});
+
+Hooks.on('renderLogo', (app,html) => {
+    if (window.hideUI) {
+        html.hide();
+    }
 });
 
 Hooks.on('renderSceneNavigation', (app,html) => {
